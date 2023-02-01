b22764a3c21ad8c05522f3345dbe0389676c3794
Leander Lindahl <leander.lindahl@ingka.ikea.com>
2023-02-01 09:39:49 +0100

    Add about section

---
+++ components/About.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+type Props = {};
+
+export default function About({}: Props) {
+  return (
+    <div
+      className={
+        'flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly items-center'
+      }
+    >
+      <h3
+        className={
+          'absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'
+        }
+      >
+        About
+      </h3>
+      <motion.img
+        initial={{ x: -200, opacity: 0 }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        src={'/images/leander-background.jpg'}
+        className={
+          '-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'
+        }
+      ></motion.img>
+    </div>
+  );
+}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
@@ -8,19 +9,47 @@ export default function Hero({}: Props) {
   const [text, count] = useTypewriter({
     words: [
       "Hello, I'm Leander Lindahl",
-      'Guy-who-loves-coffee.tsx',
-      '<ButLovesGoodUxMore />',
+      'Guy-who-loves-usability.tsx',
+      '<AndFunctionalDesign />',
     ],
     loop: true,
     delaySpeed: 2000,
   });
   return (
-    <div>
+    <div
+      className={
+        'h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'
+      }
+    >
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#f7ab0a" />
-      </h1>
+      <img
+        className={'relative rounded-full h-32 w-31 mx-auto object-cover'}
+        src={'/images/leander-norway-400px.jpg'}
+        alt={'Leander Lindahl'}
+      />
+      <div className={'z-20'}>
+        <h2 className={'text-sm uppercase text-gray-500 pb-2 tracking-[15px]'}>
+          UX Designer
+        </h2>
+        <h1 className={'text-5xl lg:text-6xl font-semibold px-10'}>
+          <span className={'mr-3'}>{text}</span>
+          <Cursor cursorColor="#f7ab0a" />
+        </h1>
+        <div className={'pt-5'}>
+          <Link href="#about">
+            <button className={'heroButton'}>About</button>
+          </Link>
+          <Link href="#experience">
+            <button className={'heroButton'}>Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className={'heroButton'}>Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className={'heroButton'}>Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
