1431c70352f3b714f286373c61c8df1f186377b8
Leander Lindahl <leander.lindahl@ingka.ikea.com>
2023-03-23 21:40:19 +0100

    Add Google Analytics G4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import Script from 'next/script';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    // <!-- Google tag (gtag.js) -->
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={'https://www.googletagmanager.com/gtag/js?id=G-7KTXQHBRRD'}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-7KTXQHBRRD');
+        `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
