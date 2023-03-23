04adebfb113efc4700c80523305577a961870604
Leander Lindahl <leander.lindahl@ingka.ikea.com>
2023-03-23 09:25:41 +0100

    Add Jummy Case Study

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 export default function Projects() {
-  const projects = [1, 2, 3, 4, 5];
+  const projects = [
+    {
+      name: 'Jummy takeaway food',
+      description: 'A UX Case Study of Jummy Takeaway Food',
+      image: 'case-studies/jummy-cover.png',
+      pdf: 'case-studies/jummy-ux-case-study.pdf',
+    },
+  ];
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -19,30 +26,35 @@ export default function Projects() {
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-6 items-center justify-center p-20 md-p-44 h-screen"
             key={index}
           >
-            <motion.img
-              initial={{ y: -300, opacity: 0 }}
-              transition={{ duration: 1.2 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              src="/images/survival.png"
-              alt="Packaging design: Survival kit"
-            />
+            <a
+              href={project.pdf}
+              title="Case Study PDF"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <motion.img
+                initial={{ y: -300, opacity: 0 }}
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                src={project.image}
+                alt={project.description}
+              />
+            </a>
             <div className="space-y-10 px-0 md:px-10 max-w-6xl">
               <h4 className="text-4xl font-semibold text-center">
-                <span className="underline decoration-[#F7AB0A]/50">
-                  Case study {index + 1} of {projects.length}:
-                </span>{' '}
-                Survival kit
+                {project.name}
               </h4>
 
               <p className="text-lg text-center md:text-left">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Aperiam blanditiis in temporibus saepe natus placeat odio,
-                repellendus, hic omnis facere praesentium non dolore ducimus ea
-                recusandae expedita voluptatem voluptates ab. Expedita, ipsa
-                odit praesentium, magnam temporibus obcaecati aliquam non illum
-                dolorem deserunt eum nobis culpa id delectus excepturi explicabo
-                sint.
+                <a
+                  href={project.pdf}
+                  title="Case Study PDF"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  UX Case Study Process Deck
+                </a>
               </p>
             </div>
           </div>
